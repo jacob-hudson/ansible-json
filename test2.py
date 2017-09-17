65ba153d489bcf4964a4f13bbf08f890b81dd5bb
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-09-16 23:03:56 -0500

    test 2 working

--- test2.py
+++ test2.py
@@ -3,9 +3,12 @@
 import json
 import os
 
-with open("example1.json", "r") as json_file:
+with open("example2.json", "r") as json_file:
     data = json.load(json_file)
 
-    for c,i in enumerate(data['Students']):
-        if i['Major'] == "Chemistry":
-            print data["Students"][c]["Name"]
+# print data['list'][2]['weather'][1]['main']
+    for c,i in enumerate(data['list']):
+        if i['dt'] == 1446940800:
+            for b,i in enumerate(data['list'][c]['weather']):
+                if i['id'] == 501:
+                    print data['list'][c]['weather'][b]['main']
