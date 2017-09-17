675c05765140bfbbd66a80949a1df93809f5640e
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-09-17 15:52:37 -0500

    correct value types

--- test2.py
+++ test2.py
@@ -19,12 +19,18 @@ with open("example2.json", "r") as json_file:
         key.append(k)
         value.append(v)
 
+    # print value[0]
+    # if value[0].isdigit():
+    #     value[0] = int(value[0])
+    #     print value[0]
 
-
+    for i,_ in enumerate(value):
+        if value[i].isdigit():
+            value[i] = int(value[i])
 
 # print data['list'][2]['weather'][1]['main']
     for c,i in enumerate(data['list']):
-        if i[key[0]] == int(value[0]):
+        if i[key[0]] == value[0]:
             for b,i in enumerate(data['list'][c]['weather']):
-                if i[key[1]] == int(value[1]):
+                if i[key[1]] == value[1]:
                     print data['list'][c]['weather'][b]['main']
