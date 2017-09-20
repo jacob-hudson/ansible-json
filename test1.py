0ef6bb32cc76d00dbd120d5eea6ccbc2ac37f4f3
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-09-19 20:05:30 -0500

    full recursion

--- test1.py
+++ test1.py
@@ -3,13 +3,47 @@
 import json
 import os
 
-with open("example1.json", "r") as json_file:
-    data = json.load(json_file)
+def recursive(data, jsonpath, k, val, value):
+    for i,v in enumerate(data):
+        if type(data) is dict:
+            if v == jsonpath:
+                return recursive(data[v], jsonpath, k, val, value)
+            elif len(data) == 1:
+                return data[v]
+            else:
+                for t in data.keys():
+                    if t == value:
+                        return data[t]
+        else: # assuming it is a list
+            for h,u in enumerate(data):
+                if data[h][k] == val:
+                    return recursive(data[h], jsonpath, k, val, value)
 
+# def non_recursive(data, jsonpath, k, val, value):
+#     for i,v in enumerate(data):
+#         if v == jsonpath:
+#             data2 = data[v]
+#             if type(data2) is list:
+#                 for h,u in enumerate(data2):
+#                     if data2[h][k] == val:
+#                         data3 = data2[h]
+#                         if type(data3) is dict:
+#                             for t in data3.keys():
+#                                 if t == value:
+#                                     return data3[t]
+
+def main():
+
+    with open("example1.json", "r") as json_file:
+        data = json.load(json_file)
+
+    jsonpath = "Students"
     search = "Major=Chemistry"
+    value = "Name"
 
-    key, value = search.split("=")
+    k, val = search.split("=")
 
-    for c,i in enumerate(data['Students']):
-        if i[key] == value:
-            print data["Students"][c]["Name"]
+    print recursive(data, jsonpath, k, val, value)
+
+if __name__ == "__main__":
+    main()
